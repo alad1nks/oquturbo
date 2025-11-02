b803f657a559c1e936abea3c0a3c58bfe012bb8d
alad1nks <vasya.sidr777@gmail.com>
2025-11-02 12:34:01 +0500

    Fix lint

---
+++ composeApp/src/jvmMain/kotlin/com/alad1nks/oquturbo/Main.kt
@@ -0,0 +1,14 @@
+package com.alad1nks.oquturbo
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() =
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "OquTurbo",
+        ) {
+            App()
+        }
+    }
