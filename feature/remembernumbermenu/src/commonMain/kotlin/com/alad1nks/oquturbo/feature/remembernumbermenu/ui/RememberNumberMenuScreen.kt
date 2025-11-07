203befb9b80c74930bd4150e0319f6e6bd3f6923
Alibi Galeyev <87616981+alad1nks@users.noreply.github.com>
2025-11-07 17:42:19 +0500

    Add RememberNumberMenuContentColumn (#13)

--- feature/remembernumbermenu/src/commonMain/kotlin/com/alad1nks/oquturbo/feature/remembernumbermenu/ui/RememberNumberMenuScreen.kt
+++ feature/remembernumbermenu/src/commonMain/kotlin/com/alad1nks/oquturbo/feature/remembernumbermenu/ui/RememberNumberMenuScreen.kt
@@ -1,16 +1,9 @@
 package com.alad1nks.oquturbo.feature.remembernumbermenu.ui
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.outlined.PlusOne
-import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material.icons.outlined.Timer
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -21,9 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.alad1nks.oquturbo.resources.AppResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -76,60 +66,28 @@ private fun RememberNumberMenuScreen(
                 ),
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = stringResource(AppResource.String.remember_number_menu_title),
-            modifier = Modifier.padding(horizontal = 16.dp),
-            fontSize = 28.sp,
-            fontWeight = FontWeight.Medium,
-        )
-
-        Spacer(modifier = Modifier.height(4.dp))
-
-        Text(
-            text = stringResource(AppResource.String.remember_number_menu_subtitle),
-            modifier = Modifier.padding(horizontal = 16.dp),
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        RememberNumberMenuItem(
-            imageVector = Icons.Outlined.Timer,
-            title = stringResource(AppResource.String.remember_number_menu_item_classic_title),
-            subtitle = stringResource(AppResource.String.remember_number_menu_item_classic_subtitle),
-            onClick = { onPlayClick(4, "0123456789") },
-            onDetailsClick = {},
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        RememberNumberMenuItem(
-            imageVector = Icons.Outlined.PlusOne,
-            title = stringResource(AppResource.String.remember_number_menu_item_classic_title),
-            subtitle = stringResource(AppResource.String.remember_number_menu_item_classic_subtitle),
-            onClick = { onPlayClick(4, "01") },
-            onDetailsClick = {},
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        RememberNumberMenuItem(
-            imageVector = Icons.Outlined.Settings,
-            title = stringResource(AppResource.String.remember_number_menu_item_classic_title),
-            subtitle = stringResource(AppResource.String.remember_number_menu_item_classic_subtitle),
-            onClick = {},
-            onDetailsClick = {},
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-        )
+        RememberNumberMenuContentColumn(onPlayClick = onPlayClick)
     }
 }
 
 @Composable
 @Preview
 private fun RememberNumberMenuScreenPreview() {
+    Scaffold {
+        RememberNumberMenuScreen(
+            showBackButton = true,
+            onBackClick = {},
+            onPlayClick = { _, _ -> },
+        )
+    }
+}
+
+@Composable
+@Preview(
+    widthDp = 800,
+    heightDp = 1280,
+)
+private fun RememberNumberMenuScreenPreviewTablet() {
     Scaffold {
         RememberNumberMenuScreen(
             showBackButton = true,
